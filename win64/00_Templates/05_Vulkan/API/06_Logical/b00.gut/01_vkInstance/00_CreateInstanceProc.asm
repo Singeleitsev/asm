e007aad0c3f69ef2c88f3c97532ca1f1cec969ab
Singeleitsev <2773555@mail.ru>
2026-06-10 16:04:38 +0300

    Swapchain

---
+++ win64/00_Templates/05_Vulkan/API/06_Logical/b00.gut/01_vkInstance/00_CreateInstanceProc.asm
@@ -0,0 +1,30 @@
+;00_CreateInstanceProc.asm
+createInstance proc
+
+;Prologue
+push rbp
+mov rbp,rsp
+
+and rsp,-16 ;Align the Stack
+sub rsp,100h ;Create the Buffer
+
+LOG_TEXT szLogVkCreateInstance
+lea rcx,createInfo_sType ;pCreateInfo
+xor rdx,rdx ;pAllocator = NULL
+lea r8,ghVkInstance ;pInstance
+call vkCreateInstance
+test eax,eax
+jnz lbl_createInstance_VkError
+LOG_TEXT szOK
+jmp lbl_createInstance_End
+
+;Errors
+lbl_createInstance_VkError:
+call SpellVkError
+
+lbl_createInstance_End:
+;Epilogue
+leave
+ret
+createInstance endp
+
